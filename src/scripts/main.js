fba188b4b2ab35ee348b59577249360763b2d041
Alina <alina90565@gmail.com>
2020-09-21 14:18:50 +0300

    added event listeners for buttons and animation for cards

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,20 @@ form.addEventListener('submit', event => {
   document.location.reload();
   event.preventDefault();
 });
+
+const localHostname = window.location.hostname;
+const buttonsLearnMore = document.querySelectorAll('.learn-more');
+
+buttonsLearnMore.forEach(button => {
+  button.addEventListener('click', event => {
+    document.location.href = localHostname + '#about-us';
+    event.preventDefault();
+  });
+});
+
+const buttonApply = document.querySelector('.button-apply');
+
+buttonApply.addEventListener('click', event => {
+  document.location.href = localHostname + '#contact-us';
+  event.preventDefault();
+});
